2b398d24d7162e9e3496eb5b17afe940b799d1e2
Matt Nichols <mattnichols206@gmail.com>
2023-10-03 14:33:04 -0700

    Add util/CanvasSketchProject w/ demo

---
+++ src/art/util/CanvasSketchProject.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import Project, { type Detail2D, type ParamChangedDetail2D } from '$lib/base/Project/Project';
+import canvasSketch from 'canvas-sketch';
+
+// canvas-sketch doesn't have TS definitions; these are just placeholders for now
+export type CanvasSketchManager = any;
+export type CanvasSketchProps = any;
+export type CanvasSketchRender = any;
+
+export default class CanvasSketchProject extends Project {
+    dimensions = [8.5, 11];
+
+    // Non-param state (to be used or overridden by subclasses)
+    ignoreKeys = ['settings', 'manager'];
+    manager: CanvasSketchManager;
+    settings = {
+        pixelsPerInch: 300,
+        units: 'in',
+        resizeCanvas: true,
+        animate: true,
+        hotkeys: false // todo; can we still enable save hotkey without enabling play toggling with space?
+    };
+
+    // Subclasses should override this method
+    sketch(initialProps: CanvasSketchProps): CanvasSketchRender {}
+
+    async init() {
+        this.manager = await canvasSketch(this.sketch.bind(this), {
+            dimensions: this.dimensions,
+            canvas: this.canvas,
+            ...this.settings
+        });
+    }
+
+    resized() {
+        this.manager?.update();
+    }
+
+    paramChanged(detail: ParamChangedDetail2D) {
+        super.paramChanged(detail);
+        if (detail.key === 'dimensions') {
+            this.manager?.update({
+                dimensions: this.dimensions
+            });
+        }
+    }
+
+    destroy(detail: Detail2D) {
+        super.destroy(detail);
+        this.manager?.unload();
+
+        // Even after unload, sketchManager will clear the canvas on resize
+        // todo: canvas-sketch PR to fix this. For now, just clear the canvas
+        this.manager.props.canvas = null;
+    }
+}
