3afc200a3e68d26fdc1b7c656f2732dc1ccf45cc
Matt Nichols <mattnichols206@gmail.com>
2024-02-12 19:28:49 -0800

    Rotation & taper for zigzags

--- src/art/RadialPatterns/RadialPatterns.ts
+++ src/art/RadialPatterns/RadialPatterns.ts
@@ -9,6 +9,9 @@ export default class RadialPatterns extends CanvasSketchProject {
     centerInset = 0.25; // "Center Inset", 0.0 to 1.0
     divisions = 20; // "Divisions", 2 to 100, step 1
     zigZagSize = 0.5; // "Zig Zag Size", 0.1 to 1
+    rotationOffset = 0; // "Rotation Offset", -1 to 1
+    insideTaper = 0; // "Inside Taper", 0 to 1
+    outsideTaper = 0; // "Outside Taper", 0 to 1
 
     sketch() {
         return (props: CanvasSketchProps) => {
@@ -22,13 +25,16 @@ export default class RadialPatterns extends CanvasSketchProject {
 
             for (let layer = 0; layer < this.divisions; layer++) {
                 const radius = (layer + 1) * gapSize;
+                const progress = layer / (this.divisions - 1);
+                const taperMultiplier =
+                    progress * (1 - this.outsideTaper) + (1 - progress) * (1 - this.insideTaper);
+                const zigZagSize = gapSize * this.zigZagSize * taperMultiplier;
 
                 const path = [];
                 for (let i = 0; i <= 100; i++) {
                     const currentRadius =
-                        maxRadius * this.centerInset +
-                        (i % 2 === 0 ? radius : radius - gapSize * this.zigZagSize);
-                    const t = i / 100;
+                        maxRadius * this.centerInset + (i % 2 === 0 ? radius : radius - zigZagSize);
+                    const t = (i + layer * this.rotationOffset) / 100;
                     const angle = t * Math.PI * 2;
                     const x = center[0] + Math.cos(angle) * currentRadius;
                     const y = center[1] + Math.sin(angle) * currentRadius;
