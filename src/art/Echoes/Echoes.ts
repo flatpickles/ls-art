e0baf329f309da2a9c0a3146d82b35dba881c8c4
Matt Nichols <mattnichols206@gmail.com>
2024-06-25 22:38:47 -0700

    Echoes completion

--- src/art/Echoes/Echoes.ts
+++ src/art/Echoes/Echoes.ts
@@ -1,18 +1,14 @@
-// todo: re-enable typescript in this file
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-
-import CanvasSketchProject, { type CanvasSketchProps } from '../util/CanvasSketchProject';
 import { renderPaths } from 'canvas-sketch-util/penplot';
+import CanvasSketchProject, { type CanvasSketchProps } from '../util/CanvasSketchProject';
 
-import PathUtil from '../util/Legacy/PathUtil';
 import type { Path } from 'd3-path';
+import PathUtil from '../util/Legacy/PathUtil';
 
 export default class Echoes extends CanvasSketchProject {
-    dRadius = 0.2; // "Radius Incr.", 0.05 to 1
-    dTheta = Math.PI / 10; // "Angle Incr.", 0 to 3.14159, step 0.001
-    gap = 0; // "Gap", 0 to 0.1, step 0.01
-    scale = 1; // "Scale", 0.1 to 1, step 0.01
+    dRadius = 0.2; // "Radius Step", 0.05 to 1
+    dTheta = Math.PI / 10; // "Angle Step", 0 to 3.14159, step 0.001
+    alignment = 0; // "Alignment", -1 to 1, step 0.01
+    scale = 1; // "Full Scale", 0.1 to 1, step 0.01
 
     sketch() {
         return (props: CanvasSketchProps) => {
@@ -25,8 +21,8 @@ export default class Echoes extends CanvasSketchProject {
 
             while (radius > this.dRadius) {
                 const path = PathUtil.approximateCircle(center, radius);
-                center[0] = center[0] + Math.cos(theta) * (dRadius - this.gap);
-                center[1] = center[1] + Math.sin(theta) * (dRadius - this.gap);
+                center[0] = center[0] + Math.cos(theta) * (dRadius * this.alignment);
+                center[1] = center[1] + Math.sin(theta) * (dRadius * this.alignment);
                 paths.push(path);
                 theta = theta + this.dTheta;
                 radius -= dRadius;
