dc69b785ba9f5bb5222c0f437ddf4bc14625068b
Matt Nichols <mattnichols206@gmail.com>
2024-08-05 15:16:23 -0700

    Shop redirect

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,16 @@
 import type { PageServerLoad, PageServerLoadEvent } from './$types';
 
+import { dev } from '$app/environment';
 import ProjectLoader from '$lib/base/ProjectLoading/ProjectLoader';
 import { settingsStore } from '$lib/base/Util/AppState';
-import { dev } from '$app/environment';
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = (async ({ cookies, request }: PageServerLoadEvent) => {
+export const load: PageServerLoad = (async ({ cookies, request, url }: PageServerLoadEvent) => {
+    // Redirect longitude.studio/shop to the Square site
+    if (url.pathname.includes('/shop')) {
+        throw redirect(301, 'https://longitude-studio.square.site/');
+    }
+
     settingsStore.loadCookies(cookies);
     const projects = await ProjectLoader.loadAvailableProjects();
     return {
